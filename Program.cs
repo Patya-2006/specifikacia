896d699eaffd5d703ecd90163ea6b0b0b78c53f7
Пухли1 <Anastasis.w@gmail.com>
2024-12-10 14:26:47 +0300

    первый коммит

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести четырехзначные числа которые при делении на 133 дают в остатке 125,а при делении на 134 дают в остатке 111:");
+                for(int i = 1000; i <= 9999; i++)
+               {
+                 if(i%133==125 && i%134==111)
+                {
+
+                Console.WriteLine(i);
+                   Console.ReadKey();
+                }
+                }
+            }
+        }
+    }
+
